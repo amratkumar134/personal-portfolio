6f9379ef179654e74eaaade24fd74579c767e4cc
Amrat kumar <83054299+amratkumar134@users.noreply.github.com>
2022-07-16 19:16:21 +0500

    Update script.js

--- personel portfolio/script.js
+++ personel portfolio/script.js
@@ -1,11 +1,1 @@
-const hamburger = document.getElementById('hamburger');
-const navUl = document.getElementById('nav-Ul');
 
-hamburger.addEventListener('click' , () => {
-  navUl.classList.toggle('show');
-});
-// Sticky Navigation Menu JS Code
-function Download(){
-  
-    document.getElementById('button').src = "amrat resume new.pdf";
-}
