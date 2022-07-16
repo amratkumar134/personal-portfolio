ce0c542284dc48cf95b2a00dc0db4fd5b475b3bc
Amrat kumar <83054299+amratkumar134@users.noreply.github.com>
2022-07-16 19:04:01 +0500

    Add files via upload

---
+++ personel portfolio/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-Ul');
+
+hamburger.addEventListener('click' , () => {
+  navUl.classList.toggle('show');
+});
+// Sticky Navigation Menu JS Code
+function Download(){
+  
+    document.getElementById('button').src = "amrat resume new.pdf";
+}
